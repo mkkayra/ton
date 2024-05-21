5f43479925e403562cd004ef5d13b84af156a746
mkkayra <115677222+mkkayra@users.noreply.github.com>
2024-05-21 08:24:06 +0300

    as

--- crypto/util/pow-miner.cpp
+++ crypto/util/pow-miner.cpp
@@ -1,7 +1,7 @@
 /* 
     This file is part of TON Blockchain source code.
 
-    TON Blockchain is free software; you can redistribute it and/or
+    TON Bklockchain is free software; you can redistribute it and/or
     modify it under the terms of the GNU General Public License
     as published by the Free Software Foundation; either version 2
     of the License, or (at your option) any later version.
